ec180a839567af8d394b2e7304d89507f3d4c633
ArtemRasskazov <rasskazov90@gmail.com>
2019-09-19 22:50:36 +0300

    Updates

---
+++ precache-manifest.f3a2982cf21fa4e278fc34eeef0bf237.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3f280995d9cf6580b3d136aad25438",
+    "url": "/japanito/index.html"
+  },
+  {
+    "revision": "288a3668820f93584d34",
+    "url": "/japanito/static/css/2.6966acc9.chunk.css"
+  },
+  {
+    "revision": "134d35f9f8fc43bbfa35",
+    "url": "/japanito/static/css/main.184fcd33.chunk.css"
+  },
+  {
+    "revision": "288a3668820f93584d34",
+    "url": "/japanito/static/js/2.96fd65c7.chunk.js"
+  },
+  {
+    "revision": "134d35f9f8fc43bbfa35",
+    "url": "/japanito/static/js/main.bdde2221.chunk.js"
+  },
+  {
+    "revision": "da833e869dd30dc160e8",
+    "url": "/japanito/static/js/runtime~main.c988e366.js"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/japanito/static/media/appStoreBadge.2928664f.svg"
+  },
+  {
+    "revision": "397d03b8382b22b167a5a7f0e85ab7cd",
+    "url": "/japanito/static/media/caret-down-solid.397d03b8.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/japanito/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/japanito/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/japanito/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/japanito/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/japanito/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f41b9fae7988fc918c9d1488e2ca3aee",
+    "url": "/japanito/static/media/googlePlayBadge.f41b9fae.png"
+  },
+  {
+    "revision": "ce4e89a0e3086de22ed0877efd91325d",
+    "url": "/japanito/static/media/pille-riin.ce4e89a0.jpg"
+  },
+  {
+    "revision": "4f86a5b2756426d765959b75a1de0765",
+    "url": "/japanito/static/media/sushi_main.4f86a5b2.png"
+  }
+]);
